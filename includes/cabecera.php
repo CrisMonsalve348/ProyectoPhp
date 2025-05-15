8af05e3819e38ef09c3b8b7ae82510fde286e5f1
Camilo5282 <jcamilo5282@gmail.com>
2025-05-15 11:28:27 -0500

    se agregaron estilos y mas modificaiones menores

--- includes/cabecera.php
+++ includes/cabecera.php
@@ -6,10 +6,52 @@
     <title>Document</title>
 </head>
 <style>
-    h1{
-        size: 40px;
-        color: red;
+    *{
+        font-family: Sans-serif;
     }
+    h1{
+        text-align: center;
+        padding: 5px;
+       
+    }
+    body{
+       
+        background-color: #fafbfd;
+    }
+    input[type="submit"]{
+        color: white;
+        background-color: #408EE0;
+        padding: 7px;
+        border-radius: 5px;
+        border: none;
+        margin: 10px;
+        width: 15%;
+        height: 30px;
+        text-align: center;
+        
+    }
+    div{
+        text-align: center;
+        background-color: white;
+        width: 35%;
+        margin: auto;
+        padding: 10px;
+        border-radius: 10px;
+        box-shadow: 4px 4px 4px grey;
+    }
+
+    form{
+        text-align: right;
+    }
+
+    input{
+        margin: 10px;
+        padding: 5px;
+        width: 95%;
+        height: 30px;
+        border-radius: 10px;
+    }
+
 </style>
 <body>
 
--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php include "includes/cabecera.php";?>
 
 
-<form action="" method="post">
+<form action="tareas.php" method="post">
 <h1>Iniciar sesion</h1>
-<input type="text" name="usuario">
-<br>
-<input type="submit">
-</form>
-<h4>Nuevo usuario?</h4>
-<a href="resgistro.php">Resgistrate aqui</a>
-
 <?php 
     if(empty($_POST["usuario"])){
-        echo "<h2>No se escribio un usuraio</h2>";
+        echo "<h4>No se escribio un usuraio</h4>";
 
     }
 
 ?>
+<input type="text" name="usuario" placeholder="Usuario">
+<br>
+<input type="submit" value="Entrar">
+</form>
+<h4>Nuevo usuario?</h4>
+<a href="registro.php">Resgistrate aqui</a>
+
+
 <?php include "includes/pie.php";?>
