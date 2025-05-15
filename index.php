716775c71645157ce711f8623e6372696614305f
Camilo5282 <jcamilo5282@gmail.com>
2025-05-15 11:46:40 -0500

    se corrigio errores

--- index.php
+++ index.php
@@ -3,16 +3,6 @@
 
 <form action="tareas.php" method="post">
 <h1>Iniciar sesion</h1>
-<<<<<<< HEAD
-<input type="text" name="usuario">
-<br>
-<input type="submit">
-</form>
-<h4>Nuevo usuario?</h4>
-<a href="registro.php">Resgistrate aqui</a>
-
-=======
->>>>>>> 8af05e3819e38ef09c3b8b7ae82510fde286e5f1
 <?php 
     if(empty($_POST["usuario"])){
         echo "<h4>No se escribio un usuraio</h4>";
